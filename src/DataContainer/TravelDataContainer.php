edee7ab75fcca958f08343c9681eaca7fc2cb32a
Tastaturberuf <mail@tastaturberuf.de>
2025-10-13 18:24:18 +0200

    Enhance alias generation in `TravelDataContainer` and enforce unique constraint for `alias` in `tc_travel` table.

--- src/DataContainer/TravelDataContainer.php
+++ src/DataContainer/TravelDataContainer.php
@@ -8,7 +8,10 @@ use Contao\CoreBundle\DependencyInjection\Attribute\AsCallback;
 use Contao\CoreBundle\Intl\Countries;
 use Contao\CoreBundle\Slug\Slug;
 use Contao\DataContainer;
+use Contao\DC_Table;
 use DigitaleDinge\TravelCatalogBundle\Model\TravelModel;
+use Doctrine\DBAL\Connection;
+use Sqids\Sqids;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 
 final readonly class TravelDataContainer
@@ -18,18 +21,39 @@ final readonly class TravelDataContainer
         private Countries $countries,
         #[Autowire(service: 'contao.slug')]
         private Slug $slugGenerator,
+        private Connection $connection,
     )
     {
     }
 
     #[AsCallback(TravelModel::TABLE, 'fields.alias.save')]
-    public function setAliasOnSave(?string $value, DataContainer $dc): string
+    public function setAliasOnSave(?string $value, DataContainer $dc): int|string
     {
-        if ($value) {
+        if (trim((string)$value) !== '') {
             return $value;
         }
 
-        return $this->slugGenerator->generate($dc->getCurrentRecord()['name']);
+        if (!$dc instanceof DC_Table) {
+            return $dc->id;
+        }
+
+        $name = $dc->getActiveRecord()['name'] ?? '';
+
+        if (trim($name) === '') {
+            throw new \Exception('The travel name is required to generate an alias.');
+        }
+
+        $alias = $this->slugGenerator->generate($name);
+
+        $statement = $this->connection
+            ->prepare('SELECT COUNT(alias) FROM tc_travel WHERE alias = ?');
+        $statement->bindValue(1, $alias);
+
+        if ($statement->executeQuery()->fetchOne() === 0) {
+            return $alias;
+        }
+
+        return sprintf('%s-%s', new Sqids('abcdefghijklmnopqrstuvwxyz', 4)->encode([$dc->id]), $alias);
     }
 
     #[AsCallback(TravelModel::TABLE, 'fields.countries.options')]
